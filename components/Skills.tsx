94cc3fb7b32380c12418b9d02ad97cf1a28e196f
anasss777 <anass2000271@gmail.com>
2024-04-07 15:48:58 +0300

    Added new skill: Firebase

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -8,103 +8,70 @@ const images = [
   {
     name: "HTML",
     imgName: "/images/html.png",
-    percentage: "100",
-    negPercentage: "0",
   },
   {
     name: "CSS",
     imgName: "/images/css.png",
-    percentage: "100",
-    negPercentage: "0",
   },
   {
     name: "JavaScript",
     imgName: "/images/js.png",
-    percentage: "100",
-    negPercentage: "0",
   },
-
   {
     name: "TypeScript",
     imgName: "/images/Typescript.png",
-    percentage: "100",
-    negPercentage: "0",
   },
   {
     name: "React.js",
     imgName: "/images/reactjs.png",
-    percentage: "100",
-    negPercentage: "0",
   },
   {
     name: "Next.js",
     imgName: "/images/nextjs.png",
-    percentage: "100",
-    negPercentage: "0",
   },
-
   {
     name: "GitHub",
     imgName: "/images/github-logo.png",
-    percentage: "100",
-    negPercentage: "0",
   },
   {
     name: "Tailwind CSS",
     imgName: "/images/tailwind-logo.png",
-    percentage: "100",
-    negPercentage: "0",
+  },
+  {
+    name: "Firebase",
+    imgName: "/images/firebase.svg",
   },
   {
     name: "NextAuth.js",
     imgName: "/images/nextauth.png",
-    percentage: "80",
-    negPercentage: "20",
   },
-
   {
     name: "Sanity (CMS)",
     imgName: "/images/sanity.png",
-    percentage: "80",
-    negPercentage: "20",
   },
   {
     name: "Node.js",
     imgName: "/images/nodejs.png",
-    percentage: "80",
-    negPercentage: "20",
   },
   {
     name: "Next-intl",
     imgName: "/images/next-intl.png",
-    percentage: "80",
-    negPercentage: "20",
   },
-
   {
     name: "Java",
     imgName: "/images/java.png",
-    percentage: "80",
-    negPercentage: "20",
   },
   {
     name: "Python",
     imgName: "/images/python.png",
-    percentage: "70",
-    negPercentage: "30",
   },
   {
     name: "C#",
     imgName: "/images/cs.png",
-    percentage: "70",
-    negPercentage: "30",
   },
-
   {
     name: "C++",
     imgName: "/images/cpp.png",
-    percentage: "70",
-    negPercentage: "30",
   },
 ];
 
